a372ebba0352f2e9e22ddae7f629d2dc04d3388f
Suresh Nandlall <suresh_nandlall@outlook.com>
2024-11-29 16:19:04 -0500

    Using config.ini to hold sensitive data.  So it is easier to share python scripts now

--- match_groups.py
+++ match_groups.py
@@ -1,15 +1,25 @@
 import argparse
 import getpass
+import configparser
 from ldap3 import Server, Connection, ALL
 
+# Read configuration file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Extract values from the configuration file
+server_name = config['DEFAULT']['server']
+user_dn = config['DEFAULT']['user_dn']
+base_dn = config['DEFAULT']['base_dn']
+
 # Define the server and connection
-server = Server('ADSWCDCPDC12', get_info=ALL)
+server = Server(server_name, get_info=ALL)
 
 # Function to get group memberships
 def get_user_groups(username, password):
-    conn = Connection(server, 'CN=nandls01,OU=NYULMC Users,DC=nyumc,DC=org', password, auto_bind=True)
+    conn = Connection(server, user_dn, password, auto_bind=True)
     search_filter = f'(sAMAccountName={username})'
-    conn.search('DC=nyumc,DC=org', search_filter, attributes=['memberOf'])
+    conn.search(base_dn, search_filter, attributes=['memberOf'])
     return [entry.split(',')[0].split('=')[1] for entry in conn.entries[0].memberOf]
 
 # Function to find common groups
